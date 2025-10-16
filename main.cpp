dc81fad8500867a8f8c9d9dc71a2ff30b22f0e9d
gabii <gabiiperezv23@gmail.com>
2025-10-16 17:13:23 -0400

    se agrega el proyecto

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+#define pi 3.1416
+using namespace std;
+
+int main()//funcion principal
+{//inicio
+    float R,h,A,V;
+    cout<<"Valor de R= ";
+    cin>>R;
+    cout<<"Valor de h= ";
+    cin>>h;
+    //en fracciones establecer con decimal (1.0/3.0)
+    A=(2*pi*R)*h;
+    V=((1.0/3.0)*pi*pow(h,2))*((R*3)-h);
+    //endl=comando para salto de linea
+    cout<<"El área del casquete esferico es  "<<A<<endl;
+    cout<<"El volumen del casquete esferico es  "<<V;
+
+    return 0;
+
+}//fin
